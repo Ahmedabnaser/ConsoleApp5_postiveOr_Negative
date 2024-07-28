7d8458ac2d52141e0930af5be459bcaad2ffaa83
ahmed abnaser <ahmeducfjf@gmail.com>
2024-07-28 05:59:44 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls enter a number ");
+            int num = int.Parse(Console.ReadLine());
+            if (num >= 0)
+                Console.WriteLine($"the number {num} is Postive");
+            else
+                Console.WriteLine($"the number {num} is Negative");
+
+        }
+    }
+}
